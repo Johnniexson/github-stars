5f09ed6ef9d39f4208b287ed1d8b0e079cf32094
Theophilus Johnson <johnniexson@gmail.com>
2025-09-30 14:58:39 +0200

    Add trending repo list component

---
+++ src/app/components/trendings/trendings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trendings',
+  standalone: true,
+  imports: [],
+  templateUrl: './trendings.component.html',
+  styleUrl: './trendings.component.css'
+})
+export class TrendingsComponent {
+
+}
