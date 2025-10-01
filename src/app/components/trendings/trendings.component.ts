8cd1620a74630fcea58aa5c54dcac159a2c3b453
Theophilus Johnson <johnniexson@gmail.com>
2025-10-01 21:49:16 +0200

    feat: implement infinite scrolling for trendings component

--- src/app/components/trendings/trendings.component.ts
+++ src/app/components/trendings/trendings.component.ts
@@ -1,26 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { GithubRepository } from '../../types';
 import { HlmDialogService } from '@spartan-ng/helm/dialog';
 import { DetailsComponent } from '../details/details.component';
 import { RepoService } from '../../services/repo.service';
 import { FormatNumberPipe } from "../../pipes/formatNumber/format-number.pipe";
-import { HlmButton } from '@spartan-ng/helm/button';
 import { LucideAngularModule, StarIcon } from 'lucide-angular';
 @Component({
   selector: 'app-trendings',
   standalone: true,
-  imports: [CommonModule, FormatNumberPipe, HlmButton, LucideAngularModule],
+  imports: [CommonModule, FormatNumberPipe, LucideAngularModule],
   templateUrl: './trendings.component.html',
   styleUrl: './trendings.component.css'
 })
-export class TrendingsComponent {
+export class TrendingsComponent implements AfterViewInit {
+  @ViewChild('anchor', { static: false }) anchor!: ElementRef<HTMLElement>;
+  private observer!: IntersectionObserver;
+
   readonly StarIcon = StarIcon;
   private readonly _hlmDialogService = inject(HlmDialogService);
   private readonly _repoService = inject(RepoService);
   isLoading = this._repoService.isLoading;
   items = this._repoService.items;
   hasNextPage = this._repoService.hasNextPage;
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && !this.isLoading() && this.hasNextPage()) {
+        console.log('loading more items...');
+        this.loadMore();
+      }
+    });
+    this.observer.observe(this.anchor.nativeElement);
+  }
 
   public viewRepoDetails(repo: GithubRepository) {
     const dialogRef = this._hlmDialogService.open(DetailsComponent, {
@@ -32,7 +44,7 @@ export class TrendingsComponent {
 
     dialogRef.closed$.subscribe((repo) => {
       if (repo) {
-        console.log('updated repo:', repo);
+        console.log('viewed repo:', repo);
       }
     });
   }
