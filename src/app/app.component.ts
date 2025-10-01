0091a21d97717c9336ef164b6269798c5b8d4ae5
Theophilus Johnson <johnniexson@gmail.com>
2025-10-01 20:32:49 +0200

    feat: update AppComponent with search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, effect } from '@angular/core';
+import { HouseIcon, LucideAngularModule } from 'lucide-angular';
+import { TrendingsComponent } from './components/trendings/trendings.component';
+import { HlmInput } from '@spartan-ng/helm/input';
+import { RepoService } from './services/repo.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, HlmInput, TrendingsComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private readonly _repoService = inject(RepoService);
   title = 'github-stars';
+  readonly HouseIcon = HouseIcon;
+
+  searchTerm = model('');
+
+  constructor() {
+    effect(() => {
+      this._repoService.updateSearchTerm(this.searchTerm());
+    });
+  }
 }
